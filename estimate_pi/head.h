08a1b643d23708aa1cfabad4f798795808ac3ddb
Marino <56511879+Gabriel-Marino@users.noreply.github.com>
2021-02-17 14:11:33 -0300

    Update head.h

--- estimate_pi/head.h
+++ estimate_pi/head.h
@@ -1,9 +1,9 @@
-//criado em				:	2020.03.03
-//ult. atualização		:	2020.03.03
-//autor					:	Marino <ra115114@uem.br>
-//notas					:	
-//compilação			:	make
-//execução				:	time ./estimate_pi.out
+//criado em				    :	2020.03.03
+//ult. atualização		:	2021.02.17
+//autor					      :	Marino <ra115114@uem.br>
+//notas					      :	
+//compilação			    :	make
+//execução				    :	time ./estimate_pi.out
 
 #include <stdio.h>
 #include <stdlib.h>
